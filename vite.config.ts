d55ed4cc52b7e9436310afe9a7dc43d96916807a
anas155 <72773833+anas155@users.noreply.github.com>
2025-05-15 01:47:32 +0100

    your commit message

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/anas155.github.io/',    // Add this line
+  base: './',    // Use relative base path for user pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
