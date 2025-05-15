6036302ed5d587d859c807a069cf9d0494fa9a81
anas155 <72773833+anas155@users.noreply.github.com>
2025-05-15 01:20:14 +0100

    Your commit message here

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/anas155.github.io/',    // Add this line
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
